9d0ecdab85f9e5a1fd7fbfeba41be5219b65356d
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-03-31 17:24:54 +0200

    feat: add form fields for title, body, and public status in post creation

--- src/App.jsx
+++ src/App.jsx
@@ -22,13 +22,20 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    console.log("Dati inviati:", post);
+
     fetch("https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts", {
       method: "POST",
       body: JSON.stringify(post),
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log("Successo:", data);
+        /*   console.log("Dati inviati:", {
+          author: data.author,
+          title: data.title,
+          body: data.body,
+          public: data.public,
+        }); */
 
         setPost({
           author: "",
@@ -56,6 +63,39 @@ function App() {
             onChange={handleInputChange}
           />
         </div>
+        <div className="mb-3">
+          <label>Titolo</label>
+          <input
+            type="text"
+            className="form-control"
+            name="title"
+            value={post.title}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label>Testo</label>
+          <textarea
+            className="form-control"
+            name="body"
+            rows="5"
+            value={post.body}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            name="public"
+            checked={post.public}
+            onChange={handleInputChange}
+          />
+          <label className="form-check-label">Post pubblico</label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Crea Post
+        </button>
       </form>
     </div>
   );
