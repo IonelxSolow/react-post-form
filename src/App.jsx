4b37467f4df61f6c546df1ff6ead972af62be645
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-03-31 15:17:01 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <h1>ciao</h1>;
+}
+
+export default App;
